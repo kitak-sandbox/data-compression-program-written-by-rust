18a55ef67d34387e7c647c0cd59bc2b23c57a197
Keisuke KITA <kei.kita2501@gmail.com>
2017-04-19 00:41:11 +0900

    ファイルを出力してみた

--- chapter1_a/src/main.rs
+++ chapter1_a/src/main.rs
@@ -7,17 +7,30 @@ use std::error::Error;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let path = Path::new(&args[1]);
-    let display = path.display();
+    let input_path = Path::new(&args[1]);
+    let input_display = input_path.display();
 
-    let mut file = match File::open(path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+    let mut file = match File::open(input_path) {
+        Err(why) => panic!("couldn't open {}: {}", input_display, why.description()),
         Ok(file) => file,
     };
 
     let mut contents = String::new();
     match file.read_to_string(&mut contents) {
-        Err(why) => panic!("couldn't read {}: {}", display, why.description()),
+        Err(why) => panic!("couldn't read {}: {}", input_display, why.description()),
         Ok(_) => print!("{}", contents),
     };
+
+    let output_path = Path::new(&args[2]);
+    let output_display = output_path.display();
+
+    let mut file = match File::create(output_path) {
+        Err(why) => panic!("couldn't create {}: {}", output_display, why.description()),
+        Ok(file) => file,
+    };
+
+    match file.write_all(contents.as_bytes()) {
+        Err(why) => panic!("couldn't write {}: {}", output_display, why.description()),
+        Ok(_) => {},
+    }
 }
